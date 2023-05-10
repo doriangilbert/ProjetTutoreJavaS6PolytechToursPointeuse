b25fe5fbb4cc6d2280f6a945883bada276d6ab7c
Epikminey <beunas.antoine@orange.fr>
2023-05-10 17:23:16 +0200

    Create TimeClock.java

---
+++ PT_JAVA_Pointeuse/src/TimeClock.java
@@ -0,0 +1,34 @@
+import java.lang.reflect.Array;
+import java.net.InetSocketAddress;
+import java.util.Map;
+
+public class TimeClock
+{
+	private float Version;
+	private InetSocketAddress IPPort;
+	private Array CheckStorage;
+	private Map EmployeeCheck;
+	
+	
+	
+	public void Autocomplete()
+	{
+		
+	}
+	
+	void RoundTime()
+	{
+		
+	}
+	
+	void DisplayTime()
+	{
+		
+	}
+	
+	void DisplayDate()
+	{
+		
+	}
+	
+}

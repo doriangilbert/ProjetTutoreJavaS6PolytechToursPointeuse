3fbdd3c2ab79aaa154bd8f87e21c6a810c09c377
Epikminey <beunas.antoine@orange.fr>
2023-05-31 15:47:10 +0200

    éléments de la pointeuse

--- PT_JAVA_Pointeuse/src/application/AppEmulateur.java
+++ PT_JAVA_Pointeuse/src/application/AppEmulateur.java
@@ -1,5 +1,8 @@
 package application;
 
+
+import java.time.LocalDateTime;
+
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
@@ -7,48 +10,109 @@ import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
-public class AppEmulateur extends Application {
-
+public class AppEmulateur extends Application
+{
 	@Override
-	public void start(Stage primaryStage) {
-		try {
+	public void start(Stage primaryStage)
+	{
+			
+		try
+		{
 			/*
 			 * BorderPane root = new BorderPane(); Scene scene = new Scene(root, 400, 400);
 			 * scene.getStylesheets().add(getClass().getResource("application.css").
 			 * toExternalForm());
 			 */
-
+			
+			//creation of a window for the TimeClock
 			GridPane grid = new GridPane();
 			grid.setAlignment(Pos.CENTER);
 			grid.setHgap(10);
 			grid.setVgap(10);
 			grid.setPadding(new Insets(25, 25, 25, 25));
 
-			Scene scene = new Scene(grid, 400, 400);
+			Scene scene = new Scene(grid, 600, 300);
 			primaryStage.setScene(scene);
 
 			primaryStage.setScene(scene);
+			primaryStage.setResizable(false);
 			primaryStage.setTitle("PT_JAVA_Pointeuse : Application Emulateur");
 			primaryStage.show();
+			
+
+			// Button for check in and check out
+			Button button = new Button("Check in/out");
+			DropShadow shadow = new DropShadow();
+			
+			/******************Display informations******************/
+			// TextArea to put the Date
+			final Label date = new Label(LocalDateTime.now().getDayOfWeek()
+					+ " " + LocalDateTime.now().getDayOfMonth()
+					+ " " + LocalDateTime.now().getMonth()
+					+ " " + LocalDateTime.now().getYear());
+	        GridPane.setConstraints(date, 1, 1);
+	        date.setFont(new Font("Arial", 18));
+	        date.setTextFill(Color.web("black"));
+	        
+	        // TextArea to put the Hour
+ 			final Label hour = new Label(LocalDateTime.now().getHour()
+ 					+ ":" + LongMinute(LocalDateTime.now().getMinute()));
+ 	        GridPane.setConstraints(hour, 1, 1);
+ 	        hour.setFont(new Font("Arial", 30));
+ 	        
+ 	        
+ 	        // TextArea to put the Approximation of the Hour
+ 			final Label roundHour = new Label(RoundHour(LocalDateTime.now().getHour(), LocalDateTime.now().getMinute()));
+ 	        GridPane.setConstraints(roundHour, 1, 1);
+ 	        roundHour.setFont(new Font("Arial", 30));
+ 	        /******************Display informations******************/
+
+ 	        /******************Shadow effect button******************/
+			// Adding the shadow when the mouse cursor is on
+			button.addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>()
+			{
+				@Override
+				public void handle(MouseEvent e)
+				{
+					button.setEffect(shadow);
+				}
+			});
+
+			// Removing the shadow when the mouse cursor is off
+			button.addEventHandler(MouseEvent.MOUSE_EXITED, new EventHandler<MouseEvent>()
+			{
+				@Override
+				public void handle(MouseEvent e)
+				{
+					button.setEffect(null);
+				}
+			});
+			/******************Shadow effect button******************/
+
+			//root.getChildren().addAll(button1);
 
 			TextField userTextField = new TextField();
 			grid.add(userTextField, 1, 1);
-			Button btn = new Button("Check in/out");
+			
 			HBox hbBtn = new HBox(10);
 			hbBtn.setAlignment(Pos.BOTTOM_RIGHT);
-			hbBtn.getChildren().add(btn);
+			hbBtn.getChildren().setAll(button, date, hour, roundHour);
 			grid.add(hbBtn, 1, 4);
 			final Text actiontarget = new Text();
 			grid.add(actiontarget, 1, 6);
 
-			btn.setOnAction(new EventHandler<ActionEvent>() {
+			button.setOnAction(new EventHandler<ActionEvent>() {
 				@Override
 				public void handle(ActionEvent e) {
 					actiontarget.setFill(Color.FIREBRICK);
@@ -58,15 +122,70 @@ public class AppEmulateur extends Application {
 					start(primaryStage);
 				}
 			});
-			
 
-		} catch (Exception e) {
+				/*
+				 * TextField userTextField = new TextField(); grid.add(userTextField, 1, 1);
+				 * Button btn = new Button("Check in/out"); HBox hbBtn = new HBox(10);
+				 * hbBtn.setAlignment(Pos.BOTTOM_RIGHT); hbBtn.getChildren().add(btn);
+				 * grid.add(hbBtn, 1, 4); final Text actiontarget = new Text();
+				 * grid.add(actiontarget, 1, 6);
+				 * 
+				 */
+
+		}
+		catch (Exception e)
+		{
 			e.printStackTrace();
 		}
 	}
 
-	public static void main(String[] args) {
+	public static void main(String[] args)
+	{
 		launch(args);
+		// System.out.println("Heure arrondie : " + getHeure());
 	}
-
+	
+	public String LongMinute(int minute)
+	{
+		String longMinute = "";
+		if (minute < 10)
+		{
+			longMinute = "0" + minute;
+		}
+		else
+		{
+			longMinute = "" + minute;
+		}
+		return longMinute;
+	}
+	
+	public String RoundHour(int hour, int minute)
+	{
+		String longHour = "";
+		if (minute < 8)
+		{
+			 longHour = hour + ":00";
+		}
+		
+		else if (minute >= 8 && minute < 23)
+		{
+			 longHour = hour + ":15";
+		}
+		
+		else if (minute >= 23 && minute < 38)
+		{
+			 longHour = hour + ":30";
+		}
+		
+		else if (minute >= 38 && minute < 53)
+		{
+			 longHour = hour + ":45";
+		}
+		
+		else
+		{
+			 longHour = (hour + 1) + ":00";
+		}
+		return longHour;
+	}
 }

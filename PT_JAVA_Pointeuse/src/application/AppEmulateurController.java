f14c07fb0b99484156709989c6b032cab28cd0a7
doriangilbert <29594904+doriangilbert@users.noreply.github.com>
2023-06-12 14:14:37 +0200

    Correction du contrôleur du bouton check in out

--- PT_JAVA_Pointeuse/src/application/AppEmulateurController.java
+++ PT_JAVA_Pointeuse/src/application/AppEmulateurController.java
@@ -6,18 +6,19 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class AppEmulateurController {
-	/*@FXML
-	private TextField textFieldCheckInOut = (TextField) AppEmulateur.root.lookup("#textFieldCheckInOut");*/
+	@FXML
+	private TextField textFieldCheckInOut;
 	
 	@FXML
 	private Button buttonCheckInOut;
 
 	@FXML
 	protected void handleButtonCheckInOutAction(ActionEvent event) {
-		/*String message = textFieldCheckInOut.getText();
+		textFieldCheckInOut = (TextField) AppEmulateur.root.lookup("#textFieldCheckInOut");
+		String message = textFieldCheckInOut.getText();
 		System.out.println("AppEmulateur : Message Sent : " + message);
 		new TCPClientMessage().send(message);
-		//textFieldCheckInOut.clear();*/
+		textFieldCheckInOut.clear();
 	}
 	
 }

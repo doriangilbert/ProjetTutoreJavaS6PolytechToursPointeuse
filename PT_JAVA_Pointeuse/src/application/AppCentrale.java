6fc32a09d86031bd878f48fea07dee34c241055f
Mathh267 <mathh267@gmail.com>
2023-06-12 15:45:35 +0200

    Serialization et Deserialization de l'app Principale.

--- PT_JAVA_Pointeuse/src/application/AppCentrale.java
+++ PT_JAVA_Pointeuse/src/application/AppCentrale.java
@@ -6,7 +6,13 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+import java.io.*;
+
+import static java.lang.Thread.sleep;
+
 public class AppCentrale extends Application {
+
+	public static Enterprise Enterprise1;
 
 	@Override
 	public void start(Stage primaryStage) {
@@ -40,7 +46,14 @@ public class AppCentrale extends Application {
 	        primaryStage.setScene(scene);
 	        primaryStage.show();
 			
-	        new Thread(new TCPServerMessage()).start(); 
+	        File Fichier=new File("Entreprise1.dat");
+			Enterprise1=new Enterprise();
+			if (Fichier.exists()){
+				Enterprise1=Deserialization("Entreprise1.dat");
+				System.out.println(Enterprise1.getName());
+			}
+			new Thread(new AppCentraleSerialization()).start();
+			new Thread(new TCPServerMessage()).start();
 	        
 			/*do {
 				String message = new TCPServerMessage().receive();
@@ -55,4 +68,29 @@ public class AppCentrale extends Application {
 	public static void main(String[] args) {
 		launch(args);
 	}
+
+	public static void Serialization(String NomFichier) throws IOException, InterruptedException {
+		while(true){
+			FileOutputStream fos = new FileOutputStream(NomFichier);
+			ObjectOutputStream oos = new ObjectOutputStream(fos);
+			oos.writeObject(Enterprise1);
+			oos.close();
+			sleep(5000);
+		}
+	}
+
+	public static Enterprise Deserialization(String NomFichier) throws IOException {
+		FileInputStream fis = new FileInputStream(NomFichier);
+		ObjectInputStream ois = new ObjectInputStream(fis);
+		Enterprise enterpriseParam = new Enterprise();
+		try{
+			enterpriseParam =(Enterprise) ois.readObject();
+		}
+		catch (ClassNotFoundException e){
+
+		}
+		ois.close();
+		return enterpriseParam;
+	}
 }
+
---
+++ PT_JAVA_Pointeuse/src/application/AppCentraleSerialization.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+import static application.AppCentrale.Enterprise1;
+import static java.lang.Thread.sleep;
+
+public class AppCentraleSerialization implements Runnable {
+
+    @Override
+    public void run() {
+            while(true){
+                try{
+                    FileOutputStream fos = new FileOutputStream("Enterprise1.dat");
+                    ObjectOutputStream oos = new ObjectOutputStream(fos);
+                    oos.writeObject(Enterprise1);
+                    oos.close();
+                    System.out.println("Sauvegarde Finalisé");
+                    sleep(5000);
+                }
+                catch (IOException | InterruptedException e){
+
+                }
+            }
+
+    }
+}

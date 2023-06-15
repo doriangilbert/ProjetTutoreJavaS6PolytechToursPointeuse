7a021f9cff52754b0f9ae793307c387a7d8a149b
doriangilbert <29594904+doriangilbert@users.noreply.github.com>
2023-06-15 12:54:20 +0200

    Modification de la gestion des employés avec affichage de la liste des employés

--- PT_JAVA_Pointeuse/src/application/ACStaffManagementController.java
+++ PT_JAVA_Pointeuse/src/application/ACStaffManagementController.java
@@ -1,6 +1,7 @@
 package application;
 
 import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -12,8 +13,7 @@ import javafx.scene.control.cell.PropertyValueFactory;
 /**
  * Class for the staff management page display!
  */
-public class ACStaffManagementController
-{
+public class ACStaffManagementController {
 
 	@FXML
 	private TableView<Employee> tableViewStaffManagement;
@@ -23,35 +23,33 @@ public class ACStaffManagementController
 	private TableColumn<Employee, String> tableColumnFirstName;
 	@FXML
 	private TableColumn<Employee, String> tableColumnLastName;
-	
+
 	@FXML
-	private void initialize()
-	{
+	private void initialize() {
 		tableColumnID.setCellValueFactory(new PropertyValueFactory<>("id"));
 		tableColumnFirstName.setCellValueFactory(new PropertyValueFactory<>("firstName"));
 		tableColumnLastName.setCellValueFactory(new PropertyValueFactory<>("lastName"));
-		
-		//TODO : Récupérer la liste des employés et gérer les départements
-		Employee employee1 = new Employee("1", "John", "Doe");
-		Employee employee2 = new Employee("2", "Jane", "Smith");
-		Employee employee3 = new Employee("3", "Bob", "Johnson");
-		
-		tableViewStaffManagement.setItems(FXCollections.observableArrayList(employee1, employee2, employee3));
 
+		ObservableList<Employee> listEmployees = FXCollections.observableArrayList();
+
+		for (Department department : CentralApplication.Enterprise1.getListDepartment()) {
+			for (Employee employee : department.getListEmployees()) {
+				listEmployees.add(employee);
+			}
+		}
+		tableViewStaffManagement.setItems(listEmployees);
 	}
-	
-	
+
 	/**
 	 * Define the function of a button to go to the employee's creation page.
 	 * 
-	 * @param	event	the type of event (a left mouse click on a button).
+	 * @param event the type of event (a left mouse click on a button).
 	 */
 	@FXML
-	protected void handleButtonCreateEmployee(ActionEvent event)
-	{
-		try
-		{
-			//** We load the FXML file to display the window with the shape of the employee's creation page **//
+	protected void handleButtonCreateEmployee(ActionEvent event) {
+		try {
+			// ** We load the FXML file to display the window with the shape of the
+			// employee's creation page **//
 			CentralApplication.root = FXMLLoader.load(getClass().getResource("ACCreateEmployeeView.fxml"));
 
 			Scene scene = new Scene(CentralApplication.root, 640, 400);
@@ -59,24 +57,22 @@ public class ACStaffManagementController
 			CentralApplication.primaryStage.setScene(scene);
 			CentralApplication.primaryStage.show();
 		}
-		
-		catch (Exception error)
-		{
+
+		catch (Exception error) {
 			error.printStackTrace();
 		}
 	}
-	
+
 	/**
 	 * Define the function of a button to go to the Main page.
 	 * 
-	 * @param	event	the type of event (a left mouse click on a button).
+	 * @param event the type of event (a left mouse click on a button).
 	 */
 	@FXML
-	protected void handleButtonBackToHome(ActionEvent event)
-	{
-		try
-		{
-			//** We load the FXML file to display the window with the shape of the Main page **//
+	protected void handleButtonBackToHome(ActionEvent event) {
+		try {
+			// ** We load the FXML file to display the window with the shape of the Main
+			// page **//
 			CentralApplication.root = FXMLLoader.load(getClass().getResource("CentralApplicationView.fxml"));
 
 			Scene scene = new Scene(CentralApplication.root, 640, 400);
@@ -84,9 +80,8 @@ public class ACStaffManagementController
 			CentralApplication.primaryStage.setScene(scene);
 			CentralApplication.primaryStage.show();
 		}
-		
-		catch (Exception error)
-		{
+
+		catch (Exception error) {
 			error.printStackTrace();
 		}
 	}
